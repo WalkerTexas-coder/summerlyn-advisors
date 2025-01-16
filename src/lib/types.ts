6e7e8db7f5308372196729fbddc90ba7ed06c611
Austin Walker <austinsrwalker@gmail.com>
2025-01-16 12:49:20 -0600

    fix: update empty object types to use object type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,14 +1,14 @@
 import type { Metadata } from "next"
 
-export type PageProps<T = {}> = {
+export type PageProps<T = object> = {
   params: T
   searchParams: { [key: string]: string | string[] | undefined }
 }
 
-export type LayoutProps<T = {}> = PageProps<T> & {
+export type LayoutProps<T = object> = PageProps<T> & {
   children: React.ReactNode
 }
 
-export interface GenerateMetadata<T = {}> {
+export interface GenerateMetadata<T = object> {
   (props: PageProps<T>): Metadata | Promise<Metadata>
 }
